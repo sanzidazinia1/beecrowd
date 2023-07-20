73d86c996802d731ae0c78e2ea12d2765162d094
sanzidazinia1 <86622547+sanzidazinia1@users.noreply.github.com>
2023-07-20 14:21:28 +0000

    accepted

--- Banknotes_1018.c
+++ Banknotes_1018.c
@@ -10,32 +10,32 @@ int main()
     two = 0, one = 0;
     // n=576
     oneH = n / 100;
-    printf("%d nota(s) de R$ 100,00 \n", oneH);
+    printf("%d nota(s) de R$ 100,00\n", oneH);
     
     n = n % 100;
     
     fifty=n/50;
-    printf("%d nota(s) de R$ 50,00 \n", fifty);
+    printf("%d nota(s) de R$ 50,00\n", fifty);
     n=n%50;//26
     
     twenty=n/20;
-    printf("%d nota(s) de R$ 20,00 \n", twenty);
+    printf("%d nota(s) de R$ 20,00\n", twenty);
     n=n%20;
     
     ten=n/10;
-    printf("%d nota(s) de R$ 10,00 \n", ten);
+    printf("%d nota(s) de R$ 10,00\n", ten);
     n=n%10;
     
     five=n/5;
-    printf("%d nota(s) de R$ 5,00 \n", five);
+    printf("%d nota(s) de R$ 5,00\n", five);
     n=n%5;
     
     two=n/2;
-    printf("%d nota(s) de R$ 2,00 \n", two);
+    printf("%d nota(s) de R$ 2,00\n", two);
     n=n%2;
     
     one=n/1;
-    printf("%d nota(s) de R$ 1,00 \n", one);
+    printf("%d nota(s) de R$ 1,00\n", one);
    
     return 0;
 }
